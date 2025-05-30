4243b6f01843b10383355f387bb0dd7b1bcb328d
dkfmfakstp <ksj20070917@gmail.com>
2025-05-30 14:38:11 +0900

    Create foods.py

---
+++ foods.py
@@ -0,0 +1,24 @@
+# foods.py (데이터 저장용)
+foods = [
+    {
+        "name": "김치찌개",
+        "category": "한식",
+        "ingredients": ["김치", "돼지고기", "두부"],
+        "description": "매콤하고 깊은 맛의 한국 전통 찌개입니다.",
+        "image": "https://cdn.pixabay.com/photo/2017/06/22/18/38/korean-food-2432252_1280.jpg"
+    },
+    {
+        "name": "스파게티 볼로네제",
+        "category": "양식",
+        "ingredients": ["파스타", "소고기", "토마토"],
+        "description": "진한 고기 소스가 매력적인 이탈리아 대표 요리입니다.",
+        "image": "https://cdn.pixabay.com/photo/2017/12/09/08/18/spaghetti-3007395_1280.jpg"
+    },
+    {
+        "name": "짜장면",
+        "category": "중식",
+        "ingredients": ["면", "돼지고기", "짜장소스"],
+        "description": "달콤 짭조름한 춘장 소스를 곁들인 중국식 면 요리입니다.",
+        "image": "https://cdn.pixabay.com/photo/2016/11/18/16/04/jajangmyeon-1839017_1280.jpg"
+    }
+]
